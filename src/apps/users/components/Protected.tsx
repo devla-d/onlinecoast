73e137e8e62700cefb83a11084d22212665e4730
samuel aniekan <samuelaniekan680@gmail.com>
2022-10-07 16:48:39 +0100

    modified

--- src/apps/users/components/Protected.tsx
+++ src/apps/users/components/Protected.tsx
@@ -1,9 +1,15 @@
 import { useAppSelector } from "@/hooks/useStore";
 import { Navigate, Outlet } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Protected = () => {
-  const user = true; //useAppSelector((state) => state.user.user);
-  return user ? <Outlet /> : <Navigate to={"/sign-in"} />;
+  const user = useAppSelector((state) => state.user.user);
+  if (user) {
+    return <Outlet />;
+  } else {
+    toast.warn("Please login");
+    return <Navigate to={"/sign-in"} />;
+  }
 };
 
 export default Protected;
